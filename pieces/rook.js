f5297337c32f0c081abc97f883c2d6bbff74f966
ethantr <ethantrossi@gmail.com>
2023-01-03 11:19:32 +1100

    Rook can now get possible moves

--- pieces/rook.js
+++ pieces/rook.js
@@ -37,8 +37,57 @@ class Rook extends Piece {
     }
 
 
-    getPossibleMoves(){
-       
+    getPossibleMoves(board, start) {
+        var moves = [];
+
+        for (let index = start.getX() - 1; index >= 0; index--) {
+            var possibleLeft = board.getSquare(index, start.getY());
+            if (this.canMove(board, start, possibleLeft) && !possibleLeft.isVacant()) {
+                moves.push(possibleLeft);
+                break;
+            } else if (this.canMove(board, start, possibleLeft)) {
+                moves.push(possibleLeft);
+            }
+            else break;
+        }
+
+        for (let index = start.getX() + 1; index < BOARD_SIZE; index++) {
+            var possibleRight = board.getSquare(index, start.getY());
+            if (this.canMove(board, start, possibleRight) && !possibleRight.isVacant()) {
+                moves.push(possibleRight);
+                break;
+            }
+            else if (this.canMove(board, start, possibleRight)) {
+                moves.push(possibleRight);
+            }
+            else break;
+        }
+
+        for (let index = start.getY() - 1; index >= 0; index--) {
+            var possibleUp = board.getSquare(start.getX(), index);
+            if (this.canMove(board, start, possibleUp) && !possibleUp.isVacant()) {
+                moves.push(possibleUp);
+                break;
+            }
+            else if (this.canMove(board, start, possibleUp)) {
+                moves.push(possibleUp);
+            }
+            else break;
+        }
+
+        for (let index = start.getY() + 1; index < BOARD_SIZE; index++) {
+            var possibleDown = board.getSquare(start.getX(), index);
+            if (this.canMove(board, start, possibleDown) && !possibleDown.isVacant()) {
+                moves.push(possibleDown);
+                break;
+            }
+            else if (this.canMove(board, start, possibleDown)) {
+                moves.push(possibleDown);
+            } else break;
+        }
+
+        return moves;
+
     }
 
 }
